648cfe5bb3f4426d124b1e04b9b2ba3320faa08e
SatpalSahota <satpalsahota@protonmail.com>
2019-01-24 00:18:51 +0000

    More dictionary work

--- Python/Chapter_6/favourite_languages.py
+++ Python/Chapter_6/favourite_languages.py
@@ -5,19 +5,29 @@ favourite_languages = {
     'phil': 'python',
 }
 
-print("Sarah's favourite language is " + 
-    favourite_languages['sarah'].title() + ".")
 
 for n, l in favourite_languages.items():
     print(n.title() + "'s favourite language is " +
     l.title() + ".")
+print("\n")
 
 for name in favourite_languages.keys():
     print(name.title())
+print("\n")
 
 for language in favourite_languages.values():
     print(language.title())
+print("\n")
+
+for name in sorted(favourite_languages.keys()):
+    print(name.title() + ", thank you for taking the poll!.")
+print("\n")
 
 if 'erin' not in favourite_languages.keys():
     print("Erin, please take our poll!")
+print("\n")
 
+print("The following languages have been mentioned:")
+for language in set(favourite_languages.values()):
+    print(language.title())
+print("\n")
