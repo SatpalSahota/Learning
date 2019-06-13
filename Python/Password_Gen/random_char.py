68cbcaafb30316e8e62e0c6930be77084bf26a69
SatpalSahota <satpalsahota@protonmail.com>
2019-06-13 15:05:32 +0100

    reduced number of outputs * 0.1

--- Python/Password_Gen/random_char.py
+++ Python/Password_Gen/random_char.py
@@ -5,5 +5,5 @@ def randomString(stringLength):
     letters = string.ascii_letters + string.punctuation
     return ''.join(random.choice(letters) for i in range(stringLength))
 
-for x in range(100):
+for x in range(10):
     print(randomString(20))
